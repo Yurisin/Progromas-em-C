e14682ae24048ab9f6596beeeb8037447ab94467
Yurisin <72422302+Yurisin@users.noreply.github.com>
2021-01-15 11:08:07 -0300

    Add files via upload

---
+++ CalculadoraDePontencia/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+  int n1,n2,n3;
+
+while(1==1){
+ printf("Informe  base: \n");
+ scanf("%d",&n1);
+
+ printf("Informe o expoente: \n");
+ scanf("%d",&n2);
+
+ n3=pow(n1,n2);
+
+ printf("Resultado eh:%d\n",n3);
+}
+
+
+
+
+    return 0;
+}
